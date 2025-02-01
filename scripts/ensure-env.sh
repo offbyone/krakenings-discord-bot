9f64cc8b3006fa6404070e3045d3b108a938adcf
Chris Rose <offline@offby1.net>
2025-02-01 10:19:22 -0800

    Framework for the commands

--- scripts/ensure-env.sh
+++ scripts/ensure-env.sh
@@ -3,12 +3,30 @@
 HERE="$(unset CDPATH && cd "$(dirname "$0")" && pwd)"
 PROJECT_ROOT="$(cd "$HERE"/.. && pwd)"
 
-if test -f "$PROJECT_ROOT/.env"; then
-    exit 0
+if ! test -f "$PROJECT_ROOT/.env"; then
+    echo "Creating .env file..."
+    cat <<'EOF' >"$PROJECT_ROOT/.env"
+APP_BOT_TOKEN=bogon
+APP_GUILD_ID=bogon
+APP_CHANNEL_ID=bogon
+APP_WEBHOOK_URL=bogon
+EOF
 fi
 
-echo "Creating .env file..."
-cat <<EOF >"$PROJECT_ROOT/.env"
-BOT_TOKEN=bogon
-GUILD_ID=bogon
+if ! test -f "$PROJECT_ROOT/config.toml"; then
+    cat <<'EOF' >"$PROJECT_ROOT/config.toml"
+[hockey]
+teams = [
+    "SEA",
+    "EDM",
+    "DET",
+]
+
+[soccer]
+teams = ["SEA", "CHI"]
+
+[football]
+teams = ["DET"]
 EOF
+
+fi
--- src/krakenings_discord_bot/__main__.py
+++ src/krakenings_discord_bot/__main__.py
@@ -1,20 +1,44 @@
+from pathlib import Path
+
 import click
+import environ
+import tomli
+
 from src.krakenings_discord_bot.bot import Bot
 
 
-@click.command()
-def main():
-    config = {
-        "BOT_TOKEN": "your_bot_token_here",
-        "GUILD_ID": "your_guild_id_here",
-        "ICAL_URL": "your_ical_url_here",
-        "CHANNEL_ID": "your_channel_id_here",
-    }
+@environ.config()
+class BotConfig:
+    bot_token = environ.var()
+    guild_id = environ.var()
+    channel_id = environ.var()
 
-    bot = Bot(config["BOT_TOKEN"], config["GUILD_ID"], "!")
+
+@environ.config()
+class WebhookConfig:
+    webhook_url = environ.var()
+
+
+@click.group()
+def main(): ...
+
+
+@main.command()
+@click.option("--config-file", type=Path, default=Path("config.toml"))
+def schedule(config_file: Path):
+    app_config: WebhookConfig = WebhookConfig.from_environ()
+    config = tomli.load(config_file.open(mode="rb"))
+    click.echo("schedule")
+
+
+@main.command()
+def bot():
+    app_config: BotConfig = BotConfig.from_environ()
+
+    bot = Bot(app_config.bot_token, app_config.guild_id, "!")
 
     # connect teams here
-    bot.run(config["BOT_TOKEN"])
+    bot.run(app_config.bot_token)
 
 
 if __name__ == "__main__":
